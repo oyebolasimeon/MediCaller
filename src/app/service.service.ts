725e1650998f63b2a1e879dab68b8df345313c1b
Simeon Oyebola ~{{blueMan}} <simeonoyekunleoyebola@gmail.com>
2023-04-02 09:42:45 +0100

    ..

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -9,7 +9,7 @@ export class ServiceService {
   constructor(private http: HttpClient) { }
 
   message(data: any) {
-    const url = 'https://example.com/api/my-endpoint'; // replace with your API endpoint
+    const url = 'https://medicmessageapi.onrender.com/'; // replace with your API endpoint
     return this.http.post(url, data);
   }
 }
