66e9cca5f6258577c68989c4556d7a43f9ec7254
Simeon Oyebola ~{{blueMan}} <simeonoyekunleoyebola@gmail.com>
2023-04-02 09:34:07 +0100

    ...

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  message(data: any) {
+    const url = 'https://example.com/api/my-endpoint'; // replace with your API endpoint
+    return this.http.post(url, data);
+  }
 }
